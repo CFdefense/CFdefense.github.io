7665b3e44a5012a8f0b9c2514e59cc4b1cf2a583
CFdefense <CFdefence@gmail.com>
2024-09-23 16:33:06 -0400

    added a few more gifs

--- src/components/ProjectContents.jsx
+++ src/components/ProjectContents.jsx
@@ -27,13 +27,13 @@ const ProjectContents = ({ title, description, imageUrl, externalLinks }) => {
           {/* External Links Section */}
           {externalLinks && (
             <Reveal>
-            <div className="flex justify-center gap-4 mt-6">
+            <div className="flex justify-center gap-3">
               {externalLinks.map((link, index) => (
                 <a key={index} href={link.url} target="_blank" rel="noopener noreferrer">
                   <img
                     src={link.imgUrl}
                     alt={`Link ${index + 1}`}
-                    className="object-cover object-scale-down rounded w-[150px] h-[150px]"
+                    className="object-cover object-scale-down rounded w-[100px] h-[150px]"
                   />
                 </a>
               ))}
@@ -49,7 +49,7 @@ const ProjectContents = ({ title, description, imageUrl, externalLinks }) => {
               <img
                 src={imageUrl}
                 alt="Project"
-                className="object-cover object-scale-down rounded w-[500px] h-[500px]"
+                className="object-cover object-scale-down rounded w-[750px] h-[750px]"
               />
             </Reveal>
           </div>
--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -12,82 +12,82 @@ const projectData = [
   {
     title: "6502 Microprocessor Compiler",
     description: "Developed a Java-based compiler that translates high-level code into executable 6502 assembly language.",
-    imageUrl: "../../media/projects/6502.png",
+    imageUrl: "../../media/projects/compiler.gif",
     externalLinks: [
-      { url: "https://example.com/link1", imgUrl: "https://via.placeholder.com/150" },
-      { url: "https://example.com/link2", imgUrl: "https://via.placeholder.com/150" }
+      { url: "https://github.com/CFdefense/CMPT432N-6502-Java-Compiler", imgUrl: "../../media/projects/github.png" },
+      { url: "https://en.wikipedia.org/wiki/MOS_Technology_6502", imgUrl: "../../media/projects/wiki.png" }
     ]
   },
   {
     title: "Chip-8 Interpreter",
     description: "Created a Python interpreter to execute Chip-8 machine code allowing for play of retro games.",
-    imageUrl: "../../media/projects/3lRyHa.png",
+    imageUrl: "../../media/projects/chippy.gif",
     externalLinks: [
-      { url: "https://example.com/link1", imgUrl: "https://via.placeholder.com/150" },
-      { url: "https://example.com/link2", imgUrl: "https://via.placeholder.com/150" }
+      { url: "https://github.com/CFdefense/Chip-8-Emulator", imgUrl: "../../media/projects/github.png" },
+      { url: "https://en.wikipedia.org/wiki/CHIP-8", imgUrl: "../../media/projects/wiki.png" }
     ]
   },
   {
     title: "Algorithm and Data Structures",
     description: "Key algorithm and data structure concepts such as graph traversal, dynamic programming, hashing, searching, etc",
-    imageUrl: "https://via.placeholder.com/500",
+    imageUrl: "../../media/projects/algorithms.gif",
     externalLinks: [
-      { url: "https://example.com/link1", imgUrl: "https://via.placeholder.com/150" },
-      { url: "https://example.com/link2", imgUrl: "https://via.placeholder.com/150" }
+      { url: "https://github.com/CFdefense/CMPT435L", imgUrl: "../../media/projects/github.png" },
+      { url: "https://www.labouseur.com/courses/algorithms/", imgUrl: "../../media/projects/wiki.png" }
     ]
   },
   {
     title: "AI Menu Alerts",
     description: "An AI-powered dining platform that sends personalized meal alerts using user feedback.",
-    imageUrl: "https://via.placeholder.com/500",
+    imageUrl: "../../media/projects/wip.png",
     externalLinks: [
-      { url: "https://example.com/link1", imgUrl: "https://via.placeholder.com/150" },
-      { url: "https://example.com/link2", imgUrl: "https://via.placeholder.com/150" }
+      { url: "https://github.com/CFdefense/MenuAlertsAI", imgUrl: "../../media/projects/github.png" },
+      { url: "https://dineoncampus.com/marist", imgUrl: "../../media/projects/marist.png" }
     ]
   },
   {
     title: "DOOM Inspired CLI Game",
     description: "A command line game which utilizes ray-tracing and collision detection to deliver DOOM-like experience.",
-    imageUrl: "https://via.placeholder.com/500",
+    imageUrl: "../../media/projects/wip.png",
     externalLinks: [
-      { url: "https://example.com/link1", imgUrl: "https://via.placeholder.com/150" },
-      { url: "https://example.com/link2", imgUrl: "https://via.placeholder.com/150" }
+      { url: "https://github.com/CFdefense/DoomGame", imgUrl: "../../media/projects/github.png" },
+      { url: "https://en.wikipedia.org/wiki/Doom_(franchise)", imgUrl: "../../media/projects/wiki.png" }
     ]
   },
   {
     title: "CRUD RESTful API",
     description: "Simple backend Node.js RESTful API to perform CRUD operations with a MongoDB database.",
-    imageUrl: "https://via.placeholder.com/500",
+    imageUrl: "../../media/projects/crud.gif",
     externalLinks: [
-      { url: "https://example.com/link1", imgUrl: "https://via.placeholder.com/150" },
-      { url: "https://example.com/link2", imgUrl: "https://via.placeholder.com/150" }
+      { url: "https://github.com/CFdefense/CRUD-API", imgUrl: "../../media/projects/github.png" },
+      { url: "https://en.wikipedia.org/wiki/Create,_read,_update_and_delete", imgUrl: "../../media/projects/wiki.png" }
     ]
   },
   {
     title: "Arch Linux Hyprland Environment",
     description: "My custom Hyprland dotfiles allowing for a 100% customizable Arch Linux desktop environment.",
-    imageUrl: "https://via.placeholder.com/500"
+    imageUrl: "../../media/projects/hypr.gif"
     ,externalLinks: [
-      { url: "https://example.com/link1", imgUrl: "https://via.placeholder.com/150" },
-      { url: "https://example.com/link2", imgUrl: "https://via.placeholder.com/150" }
+      { url: "https://github.com/CFdefense/dotfiles", imgUrl: "../../media/projects/github.png" },
+      { url: "https://wiki.archlinux.org/title/Hyprland", imgUrl: "../../media/projects/wiki.png" }
     ]
   },
   {
     title: "Windows 2019 Domain Controller",
     description: "A Virtualized Windows Server 2019 Domain Controller with network services mimicing an enterprise environment.",
-    imageUrl: "https://via.placeholder.com/500",
+    imageUrl: "../../media/projects/dh.png",
     externalLinks: [
-      { url: "https://example.com/link1", imgUrl: "https://via.placeholder.com/150" },
-      { url: "https://example.com/link2", imgUrl: "https://via.placeholder.com/150" }
+      { url: "https://www.microsoft.com/en-us/evalcenter/evaluate-windows-server-2019", imgUrl: "../../media/projects/windows.png" },
+      { url: "https://en.wikipedia.org/wiki/Windows_Server_2019", imgUrl: "../../media/projects/wiki.png" }
     ]
   },
   {
     title: "MQ Ticket Purchasing Lab",
     description: "IBM Lab Completed using Podman and IBM MQ to handle ticket purchase requests on WSL Ubuntu.",
-    imageUrl: "https://via.placeholder.com/500",
+    imageUrl: "../../media/projects/mq.png",
     externalLinks: [
-      { url: "https://example.com/link1", imgUrl: "https://via.placeholder.com/150" },
-      { url: "https://example.com/link2", imgUrl: "https://via.placeholder.com/150" }
+      { url: "https://developer.ibm.com/learningpaths/ibm-mq-badge/", imgUrl: "../../media/projects/ibm.png" },
+      { url: "https://en.wikipedia.org/wiki/IBM_MQ", imgUrl: "../../media/projects/wiki.png" }
     ]
   }
 ];
