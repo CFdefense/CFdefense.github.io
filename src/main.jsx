42ccfe61f6e0b5aa3ad7c9c0dd9b9b53498e244a
CFdefense <CFdefence@gmail.com>
2024-09-23 19:09:06 -0400

    updated router component so it doesnt 404 on refresh on ghpages

--- src/main.jsx
+++ src/main.jsx
@@ -1,32 +1,29 @@
-import { StrictMode, useEffect } from 'react'
-import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider, useLocation} from "react-router-dom";
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { HashRouter, Routes, Route } from "react-router-dom"; // Import Routes and Route
 import Projects from './pages/Projects.jsx';
 import Experience from './pages/Experience.jsx';
 import Contacts from './pages/Contacts.jsx';
 import About from './pages/About.jsx';
-import App from './App.jsx'
-import './index.css'
+import App from './App.jsx';
+import './index.css';
 import { BackgroundProvider } from './components/BackgroundProvider.jsx';
 
 /*
-  Routing of Website Links to Respective Componenets
+  Routing of Website Links to Respective Components
 */
-const router = createBrowserRouter([
-  { path: "/", element: <App />, },
-  { path: "/projects", element: <Projects />, },
-  { path: "/experience", element: <Experience />, },
-  { path: "/contacts", element: <Contacts />, },
-  { path: "/about", element: <About />, },
-]);
-
-// Mount The Root
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BackgroundProvider>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+      <HashRouter>
+        <Routes>
+          <Route path="/" element={<App />} />
+          <Route path="/projects" element={<Projects />} />
+          <Route path="/experience" element={<Experience />} />
+          <Route path="/contacts" element={<Contacts />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </HashRouter>
     </BackgroundProvider>
   </StrictMode>,
 );
